768944fe048f8310d5f0670c7c8ad77e36ac2d08
mert kaplan <mertkaplan.tr@gmail.com>
2017-05-03 01:35:58 +0300

    first commit

---
+++ src/texteditor/TextEditor.java
@@ -0,0 +1,19 @@
+
+package texteditor;
+
+import javax.swing.JFrame;
+
+public class TextEditor {
+
+
+    public static void main(String[] args) {
+      textEditorGui _edit = new textEditorGui();
+      _edit.setBounds(0,0,700,700);
+      _edit.setResizable(false);
+      _edit.setTitle("Mert Notepad");
+       _edit.setVisible(true);
+      _edit.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    }
+    
+}
